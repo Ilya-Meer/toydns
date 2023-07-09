6f0237e3b3d66e3145d65b947c6122ae1af4f281
Ilya Meerovich <ilya.meerov@gmail.com>
2023-07-08 22:50:34 -0700

    feat: parse DNS record returned in response

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -13,6 +14,16 @@ import (
 const (
 	typeARecord = uint16(1)
 	classIN     = uint16(1)
+
+	lenRecordType = int(2)
+	lenClass      = int(2)
+)
+
+var (
+	errParsingDomainName = errors.New("domain name parsing failed")
+	errParsingQuestion   = errors.New("question parsing failed")
+	errParsingRecord     = errors.New("record parsing failed")
+	errInsufficientData  = errors.New("insufficient data to fill buffer")
 )
 
 type DNSHeader struct {
@@ -24,6 +35,15 @@ type DNSHeader struct {
 	NumAdditionals uint16
 }
 
+type DNSRecord struct {
+	Name       []byte
+	RecordType uint16
+	Class      uint16
+	Ttl        uint32
+	DataLength uint16
+	Data       []byte
+}
+
 type DNSQuestion struct {
 	domainName  []byte
 	recordType  uint16
@@ -31,43 +51,37 @@ type DNSQuestion struct {
 }
 
 func buildQuery(domainName string, recordType uint16) []byte {
-	name := encodeDomain(domainName)
-	id := rand.Intn(1 << 16)
+	queryId := rand.Intn(1 << 16)
 	recursionDesiredFlag := 1 << 8
 
-	// number of bytes of domain name, record type, and class
-	questionLength := len(name) + 2 + 2
-
 	header := DNSHeader{
-		QueryID:      uint16(id),
+		QueryID:      uint16(queryId),
 		Flags:        uint16(recursionDesiredFlag),
 		NumQuestions: 1,
 	}
 
+	name := encodeDomain(domainName)
+
+	questionLength := len(name) + lenRecordType + lenClass
 	querySize := binary.Size(header) + questionLength
 	queryBuf := bytes.NewBuffer(make([]byte, 0, querySize))
 
-	err := binary.Write(queryBuf, binary.BigEndian, &header)
-	if err != nil {
-		log.Fatal("Failed to write DNS header:", err)
-	}
+	// Encode header
+	writeBinary(queryBuf, &header, "header")
 
-	err = binary.Write(queryBuf, binary.BigEndian, name)
-	if err != nil {
-		log.Fatal("Failed to write domain name:", err)
-	}
-
-	err = binary.Write(queryBuf, binary.BigEndian, recordType)
-	if err != nil {
-		log.Fatal("Failed to write record type:", err)
-	}
-
-	err = binary.Write(queryBuf, binary.BigEndian, classIN)
-	if err != nil {
-		log.Fatal("Failed to write class:", err)
-	}
+	// Encode question
+	writeBinary(queryBuf, name, "name")
+	writeBinary(queryBuf, recordType, "recordType")
+	writeBinary(queryBuf, classIN, "class")
 
 	return queryBuf.Bytes()
+}
+
+func writeBinary(buf *bytes.Buffer, data interface{}, datum string) {
+	err := binary.Write(buf, binary.BigEndian, data)
+	if err != nil {
+		log.Fatalf("Failed to write %s: %s", datum, err.Error())
+	}
 }
 
 func encodeDomain(domainName string) []byte {
@@ -85,17 +99,25 @@ func encodeDomain(domainName string) []byte {
 }
 
 type DNSResponseParser struct {
-	buffer *bytes.Buffer
+	bytes  []byte
+	offset int
 }
 
 func NewParser(response []byte) *DNSResponseParser {
-	return &DNSResponseParser{bytes.NewBuffer(response)}
+	return &DNSResponseParser{response, 0}
 }
 
 func (d *DNSResponseParser) parseHeader() (*DNSHeader, error) {
 	header := &DNSHeader{}
+	headerSize := binary.Size(header)
 
-	err := binary.Read(d.buffer, binary.BigEndian, header)
+	buf := make([]byte, headerSize)
+	_, err := d.read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Read(bytes.NewReader(buf), binary.BigEndian, header)
 	if err != nil {
 		return nil, err
 	}
@@ -104,14 +126,50 @@ func (d *DNSResponseParser) parseHeader() (*DNSHeader, error) {
 }
 
 func (d *DNSResponseParser) parseQuestion() (*DNSQuestion, error) {
-	// Read domain name
+	name, err := d.parseDomainName()
+	if err != nil {
+		return nil, err
+	}
+
+	question := &DNSQuestion{}
+
+	question.domainName = name
+
+	// Read record type
+	if err = d.readUint16(&question.recordType); err != nil {
+		return nil, err
+	}
+
+	// Read record class
+	if err = d.readUint16(&question.recordClass); err != nil {
+		return nil, err
+	}
+
+	return question, nil
+}
+
+func (d *DNSResponseParser) parseDomainName() ([]byte, error) {
 	nameParts := make([][]byte, 0)
 
 	for {
 		lenByte := make([]byte, 1)
-		_, err := d.buffer.Read(lenByte)
-		if err != nil {
-			return nil, err
+		n := copy(lenByte, d.bytes[d.offset:d.offset+1])
+		if n < len(lenByte) {
+			return nil, errParsingDomainName
+		}
+		d.offset += n
+
+		if lenByte[0]&0b11000000 == 0b11000000 {
+			pointer := d.bytes[d.offset]
+
+			name, err := d.parseCompressedName(pointer)
+			if err != nil {
+				return nil, err
+			}
+
+			nameParts = append(nameParts, name)
+			d.offset += 1
+			break
 		}
 
 		partLength := int(lenByte[0])
@@ -120,39 +178,117 @@ func (d *DNSResponseParser) parseQuestion() (*DNSQuestion, error) {
 		}
 
 		namePart := make([]byte, partLength)
-		_, err = d.buffer.Read(namePart)
-		if err != nil {
-			return nil, err
+		n = copy(namePart, d.bytes[d.offset:d.offset+len(namePart)])
+		if n < len(namePart) {
+			return nil, errParsingDomainName
 		}
+		d.offset += n
 
 		nameParts = append(nameParts, namePart)
 	}
 
 	name := bytes.Join(nameParts, []byte("."))
 
-	question := &DNSQuestion{}
-	question.domainName = name
+	return name, nil
+}
 
-	buf := make([]byte, 2)
+func (d *DNSResponseParser) parseCompressedName(pointer byte) ([]byte, error) {
+	// save current offset
+	currOffset := d.offset
+
+	// set offset to pointer byte
+	d.offset = int(pointer)
+
+	// parse domain name
+	name, err := d.parseDomainName()
+	if err != nil {
+		return nil, err
+	}
+
+	// restore offset
+	d.offset = currOffset
+
+	return name, nil
+}
+
+func (d *DNSResponseParser) parseRecord() (*DNSRecord, error) {
+	record := &DNSRecord{}
+
+	name, err := d.parseDomainName()
+	if err != nil {
+		return nil, err
+	}
+
+	record.Name = name
 
 	// Read record type
-	_, err := d.buffer.Read(buf)
-	if err != nil {
+	if err = d.readUint16(&record.RecordType); err != nil {
 		return nil, err
 	}
-	question.recordType = binary.BigEndian.Uint16(buf)
 
-	// Read record class
-	_, err = d.buffer.Read(buf)
-	if err != nil {
+	// Read record type
+	if err = d.readUint16(&record.Class); err != nil {
 		return nil, err
 	}
-	question.recordClass = binary.BigEndian.Uint16(buf)
 
-	return question, nil
+	// Read TTL
+	if err = d.readUint32(&record.Ttl); err != nil {
+		return nil, err
+	}
+
+	// Read data length
+	if err = d.readUint16(&record.DataLength); err != nil {
+		return nil, err
+	}
+
+	// Read data
+	record.Data = make([]byte, record.DataLength)
+	_, err = d.read(record.Data)
+	if err != nil {
+		return nil, errParsingRecord
+	}
+
+	return record, nil
 }
+
+func (d *DNSResponseParser) readUint16(val *uint16) error {
+	buf := make([]byte, 2)
+
+	_, err := d.read(buf)
+	if err != nil {
+		return errParsingRecord
+	}
+
+	*val = binary.BigEndian.Uint16(buf)
+
+	return nil
+}
+
+func (d *DNSResponseParser) readUint32(val *uint32) error {
+	buf := make([]byte, 4)
+
+	_, err := d.read(buf)
+	if err != nil {
+		return errParsingRecord
+	}
+
+	*val = binary.BigEndian.Uint32(buf)
+
+	return nil
+}
+
+func (d *DNSResponseParser) read(buf []byte) (int, error) {
+	n := copy(buf, d.bytes[d.offset:])
+	if n < len(buf) {
+		return n, errInsufficientData
+	}
+	d.offset += n
+
+	return n, nil
+}
+
 func main() {
-	query := buildQuery("www.example.com", typeARecord)
+	query := buildQuery("www.google.com", typeARecord)
 
 	// resolve the address for 8.8.8.8 and port 53
 	udpAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
@@ -196,4 +332,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to parse question:", err)
 	}
+
+	record, err := parser.parseRecord()
+	if err != nil {
+		log.Fatal("Failed to parse DNS record:", err)
+	}
+
+	fmt.Printf("IP address %d", record.Data)
 }
